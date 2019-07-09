65024a04ff215781b91b16abeb62b58081d3ac3f
Merek Smith <mereksmith@hotmail.com>
2019-07-09 10:50:17 -0600

    fixed prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 // prod.js - Production keys here. This should be included in the code commit.
 module.exports = {
-  googleClientID: "process.env.GOOGLE_CLIENT_ID",
-  googleClientSecret: "process.env.GOOGLE_CLIENT_SECRET",
-  mongoURI: "process.env.MONGO_URI",
-  cookieKey: "process.env.COOKIE_KEY"
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 };
